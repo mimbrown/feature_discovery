7543b9540b97f4c08a88e3ae0b38ddd4ade47c52
Michael Brown <michael.brown@rubicotech.com>
2024-05-31 17:53:58 +0530

    Update font sizes to m3

--- lib/src/widgets/content.dart
+++ lib/src/widgets/content.dart
@@ -72,7 +72,7 @@ class Content extends StatelessWidget {
                   DefaultTextStyle(
                     style: Theme.of(context)
                         .textTheme
-                        .headline4!
+                        .headlineMedium!
                         .copyWith(color: textColor),
                     child: title!,
                   ),
@@ -82,7 +82,7 @@ class Content extends StatelessWidget {
                   DefaultTextStyle(
                     style: Theme.of(context)
                         .textTheme
-                        .bodyText2!
+                        .bodyMedium!
                         .copyWith(color: textColor.withOpacity(0.9), fontSize: 18),
                     child: description!,
                   )
